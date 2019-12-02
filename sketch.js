d225b49c9c895a579e8d9cf9f748258703ad1a53
LeannaGriffith <58051256+LeannaGriffith@users.noreply.github.com>
2019-12-01 20:50:21 -0500

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,7 +1,8 @@
 var radius = 40;
 var x = 110;
-var speed = 0.5;
+var speed = 1;
 var direction = 1;
+var obstacle = [];
 
 function setup () {
   createCanvas(400, 400);
@@ -9,13 +10,18 @@ function setup () {
 
 function draw () {
   background(0);
+
   x += speed * direction;
-  if ((x > width - radius) || (x < radius)) {
-    direction = -direction;
-  }
-  if (direction == 1) {
-    ellipse(x, 60, 50, 50);
-  } else {
-    ellipse(x, 60, 50, 50);
+  // if ((x > width - radius) || (x < radius)) {
+  //   direction = -direction;
+  // }
+  // if (direction == 1) {
+  //   ellipse(x, 60, 50, 50);
+  // } else {
+  // for (i = 0; i < 5; i++) {
+  //   obstacle[i];
+    rect(x, 60, 100, 20);
+  if (x == 400) {
+    x = -100;
   }
 }
