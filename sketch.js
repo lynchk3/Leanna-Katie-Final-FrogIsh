b7fe27eeb6726f3e3abd929283a5c1e4c3ba0e4f
LeannaGriffith <58051256+LeannaGriffith@users.noreply.github.com>
2019-12-04 10:13:04 -0500

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -3,6 +3,8 @@ var x = 110;
 var speed = 1;
 var direction = 1;
 var obstacle = [];
+var characterX = 5;
+var characterY= 5;
 
 function setup () {
   createCanvas(400, 400);
@@ -10,17 +12,27 @@ function setup () {
 
 function draw () {
   background(0);
+  ellipse(characterX, characterY, 30, 30);
+
+  if (keyIsPressed && key === 'ArrowLeft') {
+    characterX -= 2;
+  }
+  if (keyIsPressed && key === 'ArrowRight') {
+    characterX += 2;
+  }
+  if (keyIsPressed && key === 'ArrowUp') {
+    characterY -= 2;
+  }
+  if (keyIsPressed && key === 'ArrowDown') {
+    characterY += 2;
+  }
 
   x += speed * direction;
-  // if ((x > width - radius) || (x < radius)) {
-  //   direction = -direction;
-  // }
-  // if (direction == 1) {
-  //   ellipse(x, 60, 50, 50);
-  // } else {
-  // for (i = 0; i < 5; i++) {
-  //   obstacle[i];
-    rect(x, 60, 100, 20);
+  rect(x, 50, 100, 20);
+  rect(x, 140, 100, 20);
+  rect(x, 230, 100, 20);
+  rect(x, 320, 100, 20);
+
   if (x == 400) {
     x = -100;
   }
