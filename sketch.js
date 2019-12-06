1a09fdba778c1c2b2e36c4dd56fed77360a318f0
LeannaGriffith <58051256+LeannaGriffith@users.noreply.github.com>
2019-12-06 11:51:05 -0500

    Collisions
    (mark helped)

--- sketch.js
+++ sketch.js
@@ -1,12 +1,14 @@
 
-var radius = 40;
+/* globals createCanvas keyIsPressed rect background fill key noLoop text textSize */
+var radius = 30;
 var a = 200;
 var b = 200;
 var speed = 2;
 var direction = 1;
-var obstacle = [];
+var obstacles = [];
 var characterX = 5;
 var characterY = 5;
+var numberOfObstacles = 7;
 
 var firstX = 110;
 var secondX = 75;
@@ -17,9 +19,36 @@ var direction2 = 1;
 
 var win = false;
 
+// true means collision, otherwise false
+function checkCollisions () {
+  for (var i = 0; i < obstacles.length; i++) {
+    var obst = obstacles[i];
+    var c = collideRectCircle(obst.x, obst.y, obst.width, obst.height, characterX, characterY, radius);
+    if (c === true) return true;
+  }
+  return false;
+}
 
 function setup () {
   createCanvas(400, 400);
+  for (var i = 0; i < numberOfObstacles; i++) {
+    obstacles.push({
+      x: 0,
+      y: 0,
+      width: 0,
+      height: 0,
+      color: 'green',
+      update: function (x, y, w, h) {
+        this.x = x;
+        this.y = y;
+        this.width = w;
+        this.height = h;
+
+        fill(this.color);
+        rect(this.x, this.y, this.width, this.height);
+      }
+    });
+  }
 }
 
 function didYouWin () {
@@ -36,7 +65,7 @@ function draw () {
   fill('red');
 
   // Move the Character
-  ellipse(characterX, characterY, 30, 30); // This is our stand-in until we figure out sprites
+  ellipse(characterX, characterY, radius, radius); // This is our stand-in until we figure out sprites
 
   if (keyIsPressed && key === 'ArrowLeft') {
     characterX -= 2;
@@ -53,19 +82,19 @@ function draw () {
 
   fill('green');
   a += speed * direction;
-  rect(a, 50, 100, 20);
-  rect(a + 300, 50, 100, 20);
-  rect(a + 100, 180, 100, 20);
-  rect(a + 300, 180, 100, 20);
-  rect(a + 350, 300, 100, 20);
+  obstacles[0].update(a, 50, 100, 20);
+  obstacles[1].update(a + 300, 50, 100, 20);
+  obstacles[2].update(a + 100, 180, 100, 20);
+  obstacles[3].update(a + 300, 180, 100, 20);
+  obstacles[4].update(a + 350, 300, 100, 20);
 
   if (a == 400) {
     a = -a;
   }
 
   b += speed * -direction;
-  rect(b, 230, 100, 20);
-  rect(b + 100, 120, 100, 20);
+  obstacles[5].update(b, 230, 100, 20);
+  obstacles[6].update(b + 100, 120, 100, 20);
 
   if (b == -100) {
     b = 400;
@@ -78,5 +107,10 @@ function draw () {
     fill(90, 50, 40);
     textSize(30);
     text('You Made It!', 100, 400);
+  }
+
+  if (checkCollisions()) {
+    textSize(30);
+    text('whoops!', 50, 50);
   }
 }
